8e9c291d4f11a3138e41ea1d56bbeb7af2cc7b54
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-09-20 11:29:34 +0200

    fix(init): only add change handlers on client

--- init.lua
+++ init.lua
@@ -6,60 +6,57 @@ if not success then return end
 
 --#region Statebag Change Handlers
 
-AddStateBagChangeHandler("cacheVehicle", "vehicle", function(bagName, key, value)
-	if not value then return end
+if not IsDuplicityVersion() then
+	AddStateBagChangeHandler("cacheVehicle", "vehicle", function(bagName, key, value)
+		if not value then return end
 
-	local networkId = tonumber(bagName:gsub("entity:", ""), 10)
-	local validEntity, timeout = false, 0
+		local networkId = tonumber(bagName:gsub("entity:", ""), 10)
+		local validEntity, timeout = false, 0
 
-	while not validEntity and timeout < 1000 do
-		validEntity = NetworkDoesEntityExistWithNetworkId(networkId)
-		timeout += 1
-		Wait(0)
-	end
+		while not validEntity and timeout < 1000 do
+			validEntity = NetworkDoesEntityExistWithNetworkId(networkId)
+			timeout += 1
+			Wait(0)
+		end
 
-	if not validEntity then
-		return print(("^5[bgarage] ^7Statebag (^3%s^7) timed out after waiting %s ticks for entity creation on %s.^0"):format(bagName, timeout, key))
-	end
+		if not validEntity then
+			return print(("^5[bgarage] ^7Statebag (^3%s^7) timed out after waiting %s ticks for entity creation on %s.^0"):format(bagName, timeout, key))
+		end
 
-	Wait(500)
+		Wait(500)
 
-	local veh = NetworkDoesEntityExistWithNetworkId(networkId) and NetworkGetEntityFromNetworkId(networkId)
-	if not veh or veh == 0 then return end
+		local vehicle = NetworkDoesEntityExistWithNetworkId(networkId) and NetworkGetEntityFromNetworkId(networkId)
+		if not vehicle or vehicle == 0 or NetworkGetEntityOwner(vehicle) ~= cache.playerId then return end
 
-	if NetworkGetEntityOwner(veh) ~= cache.playerId then return end
+		SetVehicleOnGroundProperly(vehicle)
+		SetVehicleNeedsToBeHotwired(vehicle, false)
 
-	SetVehicleOnGroundProperly(veh)
-	SetVehicleNeedsToBeHotwired(veh, false)
+		Entity(vehicle).state:set(key, nil, true)
+	end)
 
-	Entity(veh).state:set(key, nil, true)
-end)
+	AddStateBagChangeHandler("vehicleProps", "vehicle", function(bagName, key, value)
+		if not value then return end
 
-AddStateBagChangeHandler("vehicleProps", "vehicle", function(bagName, key, value)
-	if not value then return end
+		local networkId = tonumber(bagName:gsub("entity:", ""), 10)
+		local validEntity, timeout = false, 0
 
-	local networkId = tonumber(bagName:gsub("entity:", ""), 10)
-	local validEntity, timeout = false, 0
+		while not validEntity and timeout < 1000 do
+			validEntity = NetworkDoesEntityExistWithNetworkId(networkId)
+			timeout += 1
+			Wait(0)
+		end
 
-	while not validEntity and timeout < 1000 do
-		validEntity = NetworkDoesEntityExistWithNetworkId(networkId)
-		timeout += 1
-		Wait(0)
-	end
+		if not validEntity then
+			return print(("^5[bgarage] ^7Statebag (^3%s^7) timed out after waiting %s ticks for entity creation on %s.^0"):format(bagName, timeout, key))
+		end
 
-	if not validEntity then
-		return print(("^5[bgarage] ^7Statebag (^3%s^7) timed out after waiting %s ticks for entity creation on %s.^0"):format(bagName, timeout, key))
-	end
+		Wait(500)
 
-	Wait(500)
+		local vehicle = NetworkDoesEntityExistWithNetworkId(networkId) and NetworkGetEntityFromNetworkId(networkId)
+		if not vehicle or vehicle == 0 or NetworkGetEntityOwner(vehicle) ~= cache.playerId or not SetVehicleProperties(vehicle, value) then return end
 
-	local vehicle = NetworkDoesEntityExistWithNetworkId(networkId) and NetworkGetEntityFromNetworkId(networkId)
-	if not vehicle or vehicle == 0 then return end
-
-	if NetworkGetEntityOwner(vehicle) ~= cache.playerId then return end
-	if not SetVehicleProperties(vehicle, value) then return end
-
-	Entity(vehicle).state:set(key, nil, true)
-end)
+		Entity(vehicle).state:set(key, nil, true)
+	end)
+end
 
 --#endregion Statebag Change Handlers
