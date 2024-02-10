142511975ecf93e6733c4f76c8a62a69cd0be8e5
bebomusa <lj6976294@mail.com>
2024-02-10 11:25:11 -0500

    chore(fxmanifest): update version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game "gta5"
 name "bgarage"
 author "BerkieB & Bebo"
 description "Vehicle / garage management"
-version "1.1.1"
+version "1.1.2"
 repository "https://github.com/bebomusa/bgarage"
 
 shared_scripts {
