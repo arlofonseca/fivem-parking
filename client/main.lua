82799e9d8bcff06870d20d57c03a503916c7d678
vipex <101529155+vipexv@users.noreply.github.com>
2024-02-13 19:22:12 +0100

    fix(web): impound price not syncing with config

--- client/main.lua
+++ client/main.lua
@@ -19,7 +19,8 @@ lib.points.new({
         local model = type(Impound.entity) == "string" and joaat(Impound.entity) or Impound.entity
         local type = ("male" == "male") and 4 or 5
         lib.requestModel(model)
-        npc = CreatePed(type, model, Impound.entityLocation.x, Impound.entityLocation.y, Impound.entityLocation.z, Impound.entityLocation.w, false, true)
+        npc = CreatePed(type, model, Impound.entityLocation.x, Impound.entityLocation.y, Impound.entityLocation.z,
+            Impound.entityLocation.w, false, true)
         FreezeEntityPosition(npc, true)
         SetEntityInvincible(npc, true)
         SetBlockingOfNonTemporaryEvents(npc, true)
@@ -65,7 +66,8 @@ local function spawnVehicle(plate, data, coords)
     tempVehicle = plate
     lib.requestModel(data.model)
 
-    local networkVehicle = lib.callback.await("bgarage:server:spawnVehicle", false, data.model, type(coords) == "vector4" and coords, plate)
+    local networkVehicle = lib.callback.await("bgarage:server:spawnVehicle", false, data.model,
+        type(coords) == "vector4" and coords, plate)
     if not networkVehicle then
         TriggerServerEvent("bgarage:server:vehicleSpawnFailed", plate)
         tempVehicle = nil
@@ -81,7 +83,8 @@ local function spawnVehicle(plate, data, coords)
         end
     end
 
-    local vehicle = networkVehicle == 0 and 0 or not NetworkDoesEntityExistWithNetworkId(networkVehicle) and 0 or NetToVeh(networkVehicle)
+    local vehicle = networkVehicle == 0 and 0 or not NetworkDoesEntityExistWithNetworkId(networkVehicle) and 0 or
+        NetToVeh(networkVehicle)
     if not vehicle or vehicle == 0 then
         TriggerServerEvent("bgarage:server:vehicleSpawnFailed", plate, networkVehicle)
         tempVehicle = nil
@@ -130,11 +133,15 @@ RegisterNetEvent("bgarage:client:started", function()
     hasStarted = true
 end)
 
+RegisterCommand("testing2", function()
+    UIMessage("bgarage:nui:impound:price", Impound.price)
+end, false)
+
 ---Load NUI settings on player loaded
 AddEventHandler("playerSpawned", function()
     local settings = GetResourceKvpString("bgarage:nui:state:settings")
 
-    --UIMessage("bgarage:nui:impound:price", Impound.price)
+    UIMessage("bgarage:nui:impound:price", Impound.price)
 
     if settings then
         UIMessage("bgarage:nui:state:setOptions", json.decode(settings))
@@ -235,10 +242,13 @@ RegisterNuiCallback("bgarage:nui:getLocation", function(data, cb)
     end
 
     local location = lib.callback.await("bgarage:server:getParkingSpot", false)
-    local coords = data.location == "parked" and location?.xy or data.location == "outside" and lib.callback.await("bgarage:server:getVehicleCoords", false, data.plate)?.xy or nil
+    local coords = data.location == "parked" and location?.xy or
+        data.location == "outside" and lib.callback.await("bgarage:server:getVehicleCoords", false, data.plate)?.xy or
+        nil
 
     if not coords then
-        Notify(data .. location == "outside" and locale("vehicle_doesnt_exist") or locale("no_parking_spot"), 5000, "center-right", "inform", "car" or "circle-info", "#3b82f6")
+        Notify(data .. location == "outside" and locale("vehicle_doesnt_exist") or locale("no_parking_spot"), 5000,
+            "center-right", "inform", "car" or "circle-info", "#3b82f6")
         return
     end
 
@@ -314,7 +324,8 @@ RegisterCommand("v", function(_, args)
         local entity = cache.vehicle or cache.ped
         local coords = GetEntityCoords(entity)
         local heading = GetEntityHeading(entity)
-        local success, successReason = lib.callback.await("bgarage:server:setParkingSpot", false, vec4(coords.x, coords.y, coords.z, heading))
+        local success, successReason = lib.callback.await("bgarage:server:setParkingSpot", false,
+            vec4(coords.x, coords.y, coords.z, heading))
         Notify(successReason, 5000, "center-right", "success", "circle-info", "#14532d")
 
         if not success then return end
@@ -362,7 +373,8 @@ RegisterCommand("impound", function()
     local data = lib.callback.await("bgarage:server:getVehicle", false, plate) --[[@as Vehicle?]]
 
     if data then
-        local _, reason = lib.callback.await("bgarage:server:setVehicleStatus", false, "impound", plate, data.props, data.owner)
+        local _, reason = lib.callback.await("bgarage:server:setVehicleStatus", false, "impound", plate, data.props,
+            data.owner)
         Notify(reason, 5000, "center-right", "inform", "circle-info", "#3b82f6")
     end
 
@@ -394,7 +406,7 @@ RegisterCommand("givevehicle", function(_, args)
     Notify(reason, 5000, "center-right", "inform", "circle-info", "#3b82f6")
 end, Misc.useAces)
 
----@TODO: remove this command and implement a map page on the nui - maybe add a button or two that will trigger a waypoint to your parking spot/vehicle? 
+---@TODO: remove this command and implement a map page on the nui - maybe add a button or two that will trigger a waypoint to your parking spot/vehicle?
 ---Check to locate the current position of your parking spot
 RegisterCommand("findspot", function()
     if not hasStarted then return end
@@ -465,7 +477,9 @@ if Impound.textui then
             if #(GetEntityCoords(cache.ped) - Impound.markerLocation.xyz) < Impound.markerDistance then
                 if not menuOpened then
                     sleep = 0
-                    DrawMarker(Impound.marker, Impound.markerLocation.x, Impound.markerLocation.y, Impound.markerLocation.z, 0.0, 0.0, 0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, 20, 200, 20, 50, false, false, 2, true, nil, nil, false)
+                    DrawMarker(Impound.marker, Impound.markerLocation.x, Impound.markerLocation.y,
+                        Impound.markerLocation.z, 0.0, 0.0, 0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, 20, 200, 20, 50, false,
+                        false, 2, true, nil, nil, false)
                     if not shownTextUI then
                         ShowTextUI(locale("impound_show"))
                         shownTextUI = true
