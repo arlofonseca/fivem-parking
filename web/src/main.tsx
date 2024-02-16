fa353b8937c0deff1155bfd88dcc30b764d40cf0
bebomusa <lj6976294@mail.com>
2024-02-16 06:56:22 -0500

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { isEnvBrowser } from './utils/misc';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -12,3 +13,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </MantineProvider>
   </React.StrictMode>
 );
+
+// https://github.com/overextended/ox_mdt/blob/master/web/src/main.tsx#L44
+if (isEnvBrowser()) {
+  const root: HTMLElement | null = document.getElementById('root');
+
+  root!.style.backgroundImage = 'url("https://i.imgur.com/iPTAdYV.png")'; // Night time image
+  // root!.style.backgroundImage = 'url("https://i.imgur.com/3pzRj9n.png")'; // Day time image
+  root!.style.backgroundSize = 'cover';
+  root!.style.backgroundRepeat = 'no-repeat';
+  root!.style.backgroundPosition = 'center';
+}
