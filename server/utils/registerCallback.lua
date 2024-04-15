654ed5d25e1fa03e0f492fb59b5c6326207a9484
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2024-04-15 23:54:33 +0200

    fix(registerCallback): type

--- server/utils/registerCallback.lua
+++ server/utils/registerCallback.lua
@@ -1,6 +1,6 @@
 -- https://github.com/overextended/ox_mdt/blob/master/server/utils/registerCallback.lua
 ---@param event string
----@param cb fun(playerId: number, ...: any): any
+---@param cb fun(playerId: number, ...: any): ...
 local function registerCallback(event, cb)
     lib.callback.register(event, function(source, ...)
         return cb(source, ...)
