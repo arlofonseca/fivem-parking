8f1ff5b68ff03a2871fcf425aeeb27d64afa98c0
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2024-04-16 00:02:36 +0200

    tweak(init): table.insert -> faster

--- init.lua
+++ init.lua
@@ -19,17 +19,17 @@ local oxmysql, oxmysql_msg = lib.checkDependency("oxmysql", "2.9.1")
 local ox_lib, ox_lib_msg = lib.checkDependency("ox_lib", "3.16.1")
 
 if not oxmysql then
-    table.insert(cache, oxmysql_msg)
+    cache[#cache + 1] = oxmysql_msg
 end
 
 if not ox_lib then
-    table.insert(cache, ox_lib_msg)
+    cache[#cache + 1] = ox_lib_msg
 end
 
 if shared == "ox_core" and not GetResourceState("ox_inventory"):find("start") then
     local ox_inv, ox_inv_msg = lib.checkDependency("ox_inventory", "2.28.1")
     if not ox_inv then
-        table.insert(cache, ox_inv_msg)
+        cache[#cache + 1] = ox_inv_msg
     end
 end
 
