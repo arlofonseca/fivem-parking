b0200fb1454e6a91ac065953db81f3a34230fb36
bebomusa <lj6976294@mail.com>
2024-07-28 16:57:36 -0400

    chore: bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,40 +1,40 @@
-fx_version "cerulean"
-game "gta5"
+fx_version 'cerulean'
+game 'gta5'
 
-name "bGarage"
-author "BerkieB & shifu614"
-description "Vehicle garage, management, and persistence system for FiveM."
-version "1.1.9"
-repository "https://github.com/shifu614/bGarage"
-license "MIT"
+name 'bGarage'
+author 'BerkieB & shifu614'
+description 'Vehicle garage, management, and persistence system for FiveM.'
+version '1.2.1'
+repository 'https://github.com/shifu614/bGarage'
+license 'MIT'
 
-shared_script "@ox_lib/init.lua"
+shared_script '@ox_lib/init.lua'
 
-ox_lib "locale"
+ox_lib 'locale'
 
 client_scripts {
-	"client/main.lua",
+	'client/main.lua',
 }
 
 server_scripts {
-	"@oxmysql/lib/MySQL.lua",
-	"init.lua",
-	"server/main.lua",
+	'@oxmysql/lib/MySQL.lua',
+	'init.lua',
+	'server/main.lua',
 }
 
 files {
-	"client/class/*.lua",
-	"client/framework/*.lua",
-	"client/utils/*.lua",
-	"config/client.lua",
-	"config/shared.lua",
-	"locales/*.json",
+	'client/class/*.lua',
+	'client/framework/*.lua',
+	'client/utils/*.lua',
+	'config/client.lua',
+	'config/shared.lua',
+	'locales/*.json',
 }
 
 dependencies {
-	"/onesync",
-	"/server:7290",
+	'/onesync',
+	'/server:7290',
 }
 
-lua54 "yes"
-use_experimental_fxv2_oal "yes"
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
