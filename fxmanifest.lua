01948800325d86ef798a3ccd7f1b958cbf25fcc4
arlofonseca <lj6976294@mail.com>
2025-03-18 10:06:30 -0400

    chore: update version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'fivem-parking'
 author 'arlofonseca & BerkieB'
 description 'Server-side vehicle garage for FiveM'
-version '1.2.5'
+version '0.32.2'
 repository 'https://github.com/arlofonseca/fivem-parking'
 license 'MIT'
 
